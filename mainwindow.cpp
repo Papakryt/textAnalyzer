bc9867f88e941b6e0571e6abad2f0a629295c3e1
Papakryt <kryt1990@bk.ru>
2024-05-14 15:12:06 +0300

    PK1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,13 +23,13 @@ void MainWindow::on_pushButton_clicked()
     textAnalyzer textAnal(file,&flag);
     if (flag){
         ui->Res->setRowCount(count+1);
-        ui->Res->setItem(count,0,new QTableWidgetItem(QString::number(count+1)));
-        ui->Res->setItem(count,1,new QTableWidgetItem(QString(fileName)));
-        ui->Res->setItem(count,2,new QTableWidgetItem(QString::number(textAnal.strLenght())));
-        ui->Res->setItem(count,3,new QTableWidgetItem(QString::number(textAnal.countSpace())));
-        ui->Res->setItem(count,4,new QTableWidgetItem(QString::number(textAnal.countSentences())));
-        ui->Res->setItem(count,5,new QTableWidgetItem(QString::number(textAnal.countPunctuation())));
-        ui->Res->setItem(count,6,new QTableWidgetItem(QString::number(textAnal.countWords())));
+
+        ui->Res->setItem(count,0,new QTableWidgetItem(QString(fileName)));
+        ui->Res->setItem(count,1,new QTableWidgetItem(QString::number(textAnal.strLenght())));
+        ui->Res->setItem(count,2,new QTableWidgetItem(QString::number(textAnal.countSpace())));
+        ui->Res->setItem(count,3,new QTableWidgetItem(QString::number(textAnal.countSentences())));
+        ui->Res->setItem(count,4,new QTableWidgetItem(QString::number(textAnal.countPunctuation())));
+        ui->Res->setItem(count,5,new QTableWidgetItem(QString::number(textAnal.countWords())));
         count++;
     }
     else{
